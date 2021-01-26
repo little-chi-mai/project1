dd0dae9cb4ee0a68e12a1e6b420f5bfa3f7a0f4b
Mai Nguyen <chimai.dk@gmail.com>
2021-01-26 22:39:56 +1100

    working on recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,7 +5,15 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new recipe_params
-    
+
+    if @recipe.name.present?
+      @recipe.save
+      @current_user.recipes << @recipe
+      redirect_to recipe
+    else
+      flash[:error] = "Please enter the name of your recipe."
+      render new
+    end
   end
 
   def edit
@@ -14,5 +22,10 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find params[:id]
     @user = @recipe.user
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:name, :prep_time, :prep_time_unit, :cooking_time, :cooking_time_unit, :difficulty, :course, :serving, :content, :image)
   end
 end
